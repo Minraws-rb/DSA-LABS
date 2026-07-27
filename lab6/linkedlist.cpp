80c30a79aedac48f24c99e6b94011593c76eafc1
Minraws-rb <pas081bct047@wrc.edu.np>
2026-07-28 00:30:56 +0545

    Finsihed

--- lab6/linkedlist.cpp
+++ lab6/linkedlist.cpp
@@ -1,11 +1,11 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 class Node {
-public:
+    public:
     int data;
     Node* next;
-
+    
     Node(int val) {
         data = val;
         next = NULL;
@@ -13,41 +13,89 @@ public:
 };
 
 class List {
-private:
+    private:
     Node* head;
     Node* tail;
-
-public:
+    public:
     List() {
         head = tail = NULL;
     }
-    void push_front(int val) {
+    
+    void push_first(int val) {
         Node* newNode = new Node(val);
         if(head == NULL) {
             head = tail = newNode;
-            return;
         }
         else {
             newNode->next = head;
             head = newNode;
         }
     }
+    
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if(head == NULL) {
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    
+    void pop_front() {
+        if(head == NULL) {
+            cout<<"Linked list doesnt exist!!"<<endl;
+        }
+        else {
+        Node* ptr = head;
+        head = ptr->next; 
+        ptr->next = NULL; 
+        
+        delete ptr;
+     }
+    }
+    
+    void pop_back() {
+        Node* temp = head;
+        if(head == NULL) {
+            cout<<"Linked list is empty!!"<<endl;
+        }
+        else {
+            while(temp->next != tail) {
+                temp = temp->next;
+            }
+            temp->next = NULL;
+            delete tail;
+            tail = temp;
+        }
+        
+    }
+    
     void display() {
         Node* temp = head;
         while(temp != NULL) {
-            cout<<temp->data<<" ";
+            cout<< temp->data << "->";
             temp = temp->next;
         }
+        cout<<endl;
     }
-
-
 };
 
-int main() {
-    List ll; 
-    ll.push_front(1);
-    ll.push_front(2);
-    ll.push_front(3);
+int main()
+{
+    List ll;
+    ll.push_first(1);
+    ll.push_first(2);
+    ll.push_first(3);
+    ll.push_back(0);
+    ll.push_back(-1);
     ll.display();
+    ll.pop_front();
+    ll.pop_front();
+    ll.display();
+    ll.pop_back();
+    ll.display();
+
     return 0;
 }
