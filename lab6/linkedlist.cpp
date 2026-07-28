b6480e31600d021fb5498978eb6e359e1fe37a39
Minraws-rb <pas081bct047@wrc.edu.np>
2026-07-28 08:53:54 +0545

    linkedlistimplementation

--- lab6/linkedlist.cpp
+++ lab6/linkedlist.cpp
@@ -1,27 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class Node {
     public:
     int data;
     Node* next;
-    
-    Node(int val) {
-        data = val;
-        next = NULL;
-    }
+     Node(int val) {
+         data = val;
+         next = NULL;
+     }
 };
 
 class List {
-    private:
+    public:
     Node* head;
     Node* tail;
-    public:
+    
     List() {
         head = tail = NULL;
     }
     
-    void push_first(int val) {
+    void push_front(int val) {
         Node* newNode = new Node(val);
         if(head == NULL) {
             head = tail = newNode;
@@ -44,50 +43,60 @@ class List {
     }
     
     void pop_front() {
+        Node* temp = head;
         if(head == NULL) {
-            cout<<"Linked list doesnt exist!!"<<endl;
+            cout<<"empty ll"<<endl;
+            return;
         }
-        else {
-        Node* ptr = head;
-        head = ptr->next; 
-        ptr->next = NULL; 
-        
-        delete ptr;
-     }
+        else if(head == tail) {
+            delete head;
+            head = tail = NULL;
+            return;
+        }
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
     }
     
     void pop_back() {
-        Node* temp = head;
         if(head == NULL) {
-            cout<<"Linked list is empty!!"<<endl;
+            cout<<"Empty ll"<<endl;
+            return;
         }
-        else {
-            while(temp->next != tail) {
-                temp = temp->next;
-            }
-            temp->next = NULL;
-            delete tail;
-            tail = temp;
+        else if(head == tail) {
+            delete head;
+            head = tail = NULL;
+            return;
         }
+        Node* temp = head;
+        while(temp->next != tail) {
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
         
     }
-    
     void display() {
+        if(head == NULL) {
+            cout<<"empty LL"<<endl;
+            return;
+        }
         Node* temp = head;
         while(temp != NULL) {
-            cout<< temp->data << "->";
+            cout<<temp->data<<"->";
             temp = temp->next;
         }
         cout<<endl;
     }
+    
 };
 
-int main()
-{
+int main() {
     List ll;
-    ll.push_first(1);
-    ll.push_first(2);
-    ll.push_first(3);
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
     ll.push_back(0);
     ll.push_back(-1);
     ll.display();
